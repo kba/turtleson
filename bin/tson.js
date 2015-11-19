066b74f683ca22441db062e79ce0280808d5da93
Konstantin Baierer <unixprog@gmail.com>
2015-11-19 11:03:30 +0100

    tiny bin script 'tson' to convert TSON->JSON

---
+++ bin/tson.js
@@ -0,0 +1,15 @@
+#!env node
+var TSON = require('../lib')
+var util = require('util');
+args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('Usage: tson [--pretty] /path/to/file.tson');
+} else if (args[0] === '--pretty') {
+  var obj = TSON.load(args[1]);
+  console.log(util.inspect(obj, {
+    "colors": true
+  }));
+} else {
+  var obj = TSON.load(args[0]);
+  console.log(JSON.stringify(obj, null, 2));
+}
